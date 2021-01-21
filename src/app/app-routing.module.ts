830b0034ca79c57bbed6e4fa21854af439e55a5e
TATSIANA <tannysha1987@mail.ru>
2021-01-21 08:49:03 +0300

    routing,modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: 'about', component: AboutPageComponent, children: [
-      {path: 'extra', component: AboutExtraPageComponent}
-    ]},
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
