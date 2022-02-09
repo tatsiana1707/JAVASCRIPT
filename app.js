c0e71acf91316da50f4da19091521f36e1a2612b
tatsiana1707 <52006853+tatsiana1707@users.noreply.github.com>
2022-02-09 14:20:19 +0300

    init: create board changing color

---
+++ app.js
@@ -0,0 +1,27 @@
+const board = document.querySelector('#board')
+const colors = ['#7C1F7C', '#A64100', '#062270', '#FF0000', '#008500', '#85004B', '#1240AB', '#A60000']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
